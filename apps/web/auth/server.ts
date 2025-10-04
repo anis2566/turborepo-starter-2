18499f3293cab065b8e4db7c83bf317d475bbb6a
Basic <anis2566>
2025-10-04 15:48:23 +0600

    fix-starter

--- apps/web/auth/server.ts
+++ apps/web/auth/server.ts
@@ -7,11 +7,16 @@ import { initAuth } from "@workspace/auth";
 
 import { env } from "../env";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl =
+  env.VERCEL_ENV === "production"
+    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : env.VERCEL_ENV === "preview"
+      ? `https://${env.VERCEL_URL}`
+      : "http://localhost:3000";
 
 export const auth = initAuth({
   baseUrl,
-  productionUrl: `https://"turbo.t3.gg"}`,
+  productionUrl: `https://${env.VERCEL_PROJECT_PRODUCTION_URL ?? "turbo.t3.gg"}`,
   secret: env.AUTH_SECRET,
 });
 
--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 
 export const authEnv = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
     AUTH_SECRET: z.string().min(1),
     NODE_ENV: z
       .enum(["development", "production", "test"])
@@ -11,7 +10,6 @@ export const authEnv = createEnv({
   },
   client: {},
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     AUTH_SECRET: process.env.AUTH_SECRET,
     NODE_ENV: process.env.NODE_ENV,
   },
