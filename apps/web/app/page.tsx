0881a5f90f4fb079d57578bdf7a8c0657346615f
Basic <anis2566>
2025-10-04 18:15:16 +0600

    second

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,10 @@
-"use client";
-
-import { useTRPC } from "@/trpc/react";
-import { useQuery } from "@tanstack/react-query";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.exam.all.queryOptions());
-
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World {data}</h1>
+        <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
       </div>
     </div>
--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,8 @@
 import { authRouter } from "./routers/auth";
-import { examRouter } from "./routers/exam";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
-    exam: examRouter
 });
 
 // export type definition of API
